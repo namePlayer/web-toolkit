9c679b511e52f616044218415521d20ee6987fe6
namePlayer <r06sollfrank@gmail.com>
2023-03-11 13:37:18 +0100

    Expanded Logging

--- public/index.php
+++ public/index.php
@@ -2,10 +2,14 @@
 session_start();
 
 require_once __DIR__.'/../vendor/autoload.php';
+
 App\Software::initEnvironment();
 
-const MESSAGES = new \App\PlatesExtension\Message\MessageList();
-
 require_once __DIR__.'/../config/container.php';
+
+define("MESSAGES", new \App\PlatesExtension\Message\MessageList(
+    $container->get(\Monolog\Logger::class))
+);
+
 
 require_once __DIR__.'/../config/routes.php';
--- src/App/PlatesExtension/Message/MessageList.php
+++ src/App/PlatesExtension/Message/MessageList.php
@@ -2,22 +2,34 @@
 
 namespace App\PlatesExtension\Message;
 
+use Monolog\Logger;
+
 class MessageList
 {
 
     private array $messageList = [];
 
+    public function __construct(private readonly Logger $logger)
+    {
+    }
+
     public function add(string $type, string $message): void
     {
-        $this->messageList[] =
-            [
-                'type' => $type,
-                'message' => $message
-            ];
+
+        $messageArray = [
+            'type' => $type,
+            'message' => $message
+        ];
+
+        $this->logger->debug("Added new Message (MessageList.php)", $messageArray);
+
+        $this->messageList[] = $messageArray;
     }
 
     public function getAll(): array
     {
+        $this->logger->debug("Getting Message Array (MessageList.php)");
+
         return $this->messageList;
     }
 
@@ -31,6 +43,8 @@ class MessageList
                 $count++;
             }
         }
+        $this->logger->debug("Counted all Messages with type $type :: $count (MessageList.php)");
+
         return $count;
     }
 
