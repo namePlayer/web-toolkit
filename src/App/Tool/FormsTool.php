d81c3eed7265defd02af0cb1fa1f76b4358e6d38
namePlayer <r06sollfrank@gmail.com>
2023-05-10 19:34:52 +0200

    Informations for Forms Tool

---
+++ src/App/Tool/FormsTool.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Tool;
+
+class FormsTool
+{
+
+    /*
+ * Default Information for Tool
+ * */
+    public const TOOL_DATABASE_ID = 2;
+    public const TOOL_URL = '';
+    public const TOOL_VERSION = '1.0';
+    public const TOOL_BETA = true;
+
+}
