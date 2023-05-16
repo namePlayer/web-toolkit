2ee1c0710c258f48f3741342c00e8efd3ac8e4ce
namePlayer <r06sollfrank@gmail.com>
2023-05-16 21:42:57 +0200

    Tables Tool Main Page Design

--- template/forms/mainPage.php
+++ template/forms/mainPage.php
@@ -20,7 +20,41 @@
 
     <div class="row">
 
-        <h4>Deine Formulare:</h4>
+        <table class="table table-striped table-hover">
+            <thead>
+            <tr>
+                <th scope="col">
+                    <?= $this->translate('forms-tool-form-list-title-heading') ?>
+                </th>
+                <th scope="col">
+                    <?= $this->translate('forms-tool-form-list-created-heading') ?>
+                </th>
+                <th scope="col">
+                    <?= $this->translate('forms-tool-form-list-edited-heading') ?>
+                </th>
+                <th scope="col">
+                    <?= $this->translate('forms-tool-form-list-visibility-heading') ?>
+                </th>
+                <th scope="col">&nbsp;</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Bestellung eines T-Shirts</th>
+                    <td>01.05.2023 12:34</td>
+                    <td>16.05.2023 21:30</td>
+                    <td>Privat</td>
+                    <td>
+                        <a href="#" class="text-decoration-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                            </svg>
+                            Bearbeiten
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
     </div>
 
