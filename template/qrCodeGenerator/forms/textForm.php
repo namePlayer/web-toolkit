6db64f9dfee11577ca2ce8052b7d957e584892e0
namePlayer <r06sollfrank@gmail.com>
2023-12-09 13:32:30 +0100

    QR Code Generator: Creation Page Basic Layout

---
+++ template/qrCodeGenerator/forms/textForm.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-12">
+        <div class="mb-3">
+            <label for="qrcodeGeneratorTextFormTextareaInput" class="form-label">Text</label>
+            <textarea name="qrcodeGeneratorTextFormTextareaInput" id="qrcodeGeneratorTextFormTextareaInput" class="form-control"></textarea>
+        </div>
+    </div>
+</div>
--- template/qrCodeGenerator/generate.php
+++ template/qrCodeGenerator/generate.php
@@ -7,11 +7,64 @@
             <h3><?= $this->e($this->translate('qrcode-generator-creation-pane-title')) ?></h3>
         </div>
         <div class="col-8 d-flex align-items-center">
-
+            <?= $this->insert('qrCodeGenerator/presetSelection', ['tool' => $tool]) ?>
         </div>
     </div>
 
     <div class="row">
+
+        <div class="col-md-8">
+
+            <form action="" method="post">
+                <div class="card mb-3">
+
+                    <div class="card-body">
+
+                        <?= $this->insert('qrCodeGenerator/forms/textForm') ?>
+
+                        <div class="row">
+                            <div class="col-8">
+                            </div>
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary w-100">
+                                    <?= $this->e($this->translate('qrcode-generator-creation-create-button')) ?>
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="accordion" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                <?= $this->e($this->translate('qrcode-generator-creation-customize-pane-title')) ?>
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+
+        <div class="col-md-4">
+
+            <div class="card">
+                <div class="card-body">
+
+
+
+                </div>
+            </div>
+
+        </div>
 
     </div>
 
