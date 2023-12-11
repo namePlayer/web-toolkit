106382dc90f1bee522ed341a4a2fb2cee0ec2fa4
namePlayer <r06sollfrank@gmail.com>
2023-12-11 17:46:35 +0100

    QR Code Generator: UI Adjustments

--- template/qrCodeGenerator/generate.php
+++ template/qrCodeGenerator/generate.php
@@ -56,12 +56,28 @@
 
         <div class="col-md-4">
 
-            <div class="card">
+            <div class="card mb-3">
                 <div class="card-body">
 
                     <?php if(!empty($qrCode)): ?>
 
                         <img class="w-100" src="<?= $qrCode ?>">
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8">
+                                <a href="#" class="btn btn-primary w-100" role="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                    </svg>
+                                    Download (PNG)
+                                </a>
+                            </div>
+                            <div class="col-md-2"></div>
+                        </div>
 
                     <?php else: ?>
 
