e1e3ac9c918f53c163cf30920544ecb96128f5b5
namePlayer <r06sollfrank@gmail.com>
2024-04-24 13:08:51 +0200

    Page simplification

--- template/support/supportTicket.php
+++ template/support/supportTicket.php
@@ -5,22 +5,6 @@
 <div class="container mt-3">
 
     <div class="row">
-        <div class="col-12">
-            <h2><?= $this->e($this->translate('support-tab-title')) ?></h2>
-            <?php if($this->getAccountInformation()['business'] !== NULL): ?>
-                <small class="text-muted">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                    </svg>
-                    <?= $this->e($this->translate('managed-by-organisation-extended-information-settings')) ?> <a href="/authentication/account/organisation"><?= $this->e($this->translate('learn-more')) ?></a>
-                </small>
-            <?php endif; ?>
-        </div>
-    </div>
-    <hr>
-
-    <div class="row mt-3">
 
         <div class="col-12">
             <?php $this->insert('element/alert') ?>
@@ -28,44 +12,45 @@
 
         <div class="row col-12">
 
-            <div class="col-12 mb-3">
-                <h2><?= $ticketData['title'] ?></h2>
+            <div class="col-6 mb-3">
+                <h4 class="align-items-center"><?= $ticketData['title'] ?></h4>
+                <p class="text-muted align-items-center">Ticket #<?= $ticketData['id'] ?></p>
             </div>
 
-            <div class="col-md-4 mb-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h3>Erstellt am</h3>
-                        <span><?= (new DateTime($ticketData['created']))->format('d.m.Y H:i') ?></span>
+            <div class="col-6 row">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <b>Erstellt am</b>
+                            <span><?= (new DateTime($ticketData['created']))->format('d.m.Y H:i') ?></span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h3>Letztes Update</h3>
-                        <span><?= (new DateTime($ticketData['updated']))->format('d.m.Y H:i') ?></span>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <b>Letztes Update</b>
+                            <span><?= (new DateTime($ticketData['updated']))->format('d.m.Y H:i') ?></span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h3>Status</h3>
-                        <?php if($ticketData['status'] === 0): ?>
-                            <span class="badge rounded-pill text-bg-success">Eröffnet</span>
-                        <?php elseif ($ticketData['status'] === 1): ?>
-                            <span class="badge rounded-pill text-bg-warning">On-Hold</span>
-                        <?php elseif ($ticketData['status'] === 2): ?>
-                            <span class="badge rounded-pill text-bg-danger">Geschlossen</span>
-                        <?php endif; ?>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <b>Status</b> <br>
+                            <?php if($ticketData['status'] === 0): ?>
+                                <span class="badge rounded-pill text-bg-success">Eröffnet</span>
+                            <?php elseif ($ticketData['status'] === 1): ?>
+                                <span class="badge rounded-pill text-bg-warning">On-Hold</span>
+                            <?php elseif ($ticketData['status'] === 2): ?>
+                                <span class="badge rounded-pill text-bg-danger">Geschlossen</span>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-12 mb-3 mt-3">
-                <hr>
-            </div>
+            <hr>
 
             <div class="col-1"></div>
             <div class="col-10">
